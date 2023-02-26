92b17044c0034b8f8a4acb3ec9a11dcbae2f1597
Dani Setien <dani.setien@gmail.com>
2023-02-26 14:09:11 +0100

    Add bootstrap & styled signUpForm

--- src/components/SignUpForm/signUpForm.tsx
+++ src/components/SignUpForm/signUpForm.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { RegisterUserRequest } from "../../types/types";
 import useApi from "../../hooks/useApi";
-import SignUpFormStyled from "./signUpFormStyled";
 
 const SignUpForm = (): JSX.Element => {
   const { registerUser } = useApi();
@@ -40,58 +39,95 @@ const SignUpForm = (): JSX.Element => {
       file: { image: imageValue },
     };
 
+    if (!usernameValue || !emailValue || !passwordValue) {
+      return;
+    }
+
     setUsernameValue("");
-    setImageValue("");
     setPasswordValue("");
     setEmailValue("");
+    setImageValue("");
 
     registerUser(newUser);
   };
 
   return (
-    <SignUpFormStyled>
-      <h3 className="form-title">Register: </h3>
-      <form className="form" autoComplete="off" onSubmit={handleSubmit}>
-        <label htmlFor="username">Username: </label>
-        <input
-          type="text"
-          id="username"
-          onChange={handleUsernameChange}
-          value={usernameValue}
-          placeholder="username"
-          required
-        ></input>
-        <label htmlFor="image">Image: </label>
-        <input
-          type="file"
-          id="image"
-          onChange={handleImageChange}
-          value={imageValue}
-          placeholder="image file"
-        ></input>
-        <label htmlFor="password">Password: </label>
-        <input
-          type="text"
-          id="password"
-          onChange={handlePasswordChange}
-          value={passwordValue}
-          placeholder="password"
-          required
-        ></input>
-        <label htmlFor="email">Email: </label>
-        <input
-          type="text"
-          id="email"
-          onChange={handleEmailChange}
-          value={emailValue}
-          placeholder="email"
-          required
-        ></input>
-        <button type="submit" className="form__button">
-          Sign up
-        </button>
-      </form>
-    </SignUpFormStyled>
+    <section className="vh-100" style={{ color: "#508bfc" }}>
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div
+              className="card shadow-2-strong"
+              style={{ borderRadius: "1rem" }}
+            >
+              <form
+                className="card-body p-5 text-center"
+                autoComplete="off"
+                onSubmit={handleSubmit}
+              >
+                <h3 className="mb-5">Register</h3>
+
+                <div className="form-outline mb-4">
+                  <input
+                    type="email"
+                    id="typeEmailX-2"
+                    className="form-control form-control-lg"
+                    onChange={handleEmailChange}
+                    value={emailValue}
+                    required
+                  />
+                  <label className="form-label">Email</label>
+                </div>
+
+                <div className="form-outline mb-4">
+                  <input
+                    type="username"
+                    id="typeUsernameX-2"
+                    className="form-control form-control-lg"
+                    onChange={handleUsernameChange}
+                    value={usernameValue}
+                    required
+                  />
+                  <label className="form-label">Username</label>
+                </div>
+
+                <div className="form-outline mb-4">
+                  <input
+                    type="password"
+                    id="typePasswordX-2"
+                    className="form-control form-control-lg"
+                    onChange={handlePasswordChange}
+                    value={passwordValue}
+                    required
+                  />
+                  <label className="form-label">Password</label>
+                </div>
+
+                <div className="form-outline mb-4">
+                  <input
+                    type="file"
+                    id="typeImageX-2"
+                    className="form-control form-control-lg"
+                    onChange={handleImageChange}
+                    value={emailValue}
+                  />
+                  <label className="form-label">Avatar</label>
+                </div>
+
+                <button
+                  className="btn btn-primary btn-lg btn-block"
+                  type="submit"
+                >
+                  Register
+                </button>
+
+                <hr className="my-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
